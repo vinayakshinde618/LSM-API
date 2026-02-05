99c278c88a99fd16ff945711a4ea217398252b4d
vinayakshinde618 <vinayakshinde618@gmail.com>
2026-02-05 18:13:55 +0530

    fix: server

--- server.js
+++ server.js
@@ -6,28 +6,8 @@ require('dotenv').config();
 const server = http.createServer(app);
 const env = process.env.ENV || 'dev';
 const config = require('./src/app/config/config-sample.json')[env];
-const PORT = config.PORT;
+const PORT = process.env.PORT || 1000;
 
-// server.listen(PORT, () => {
-//   console.warn(`Academics and content is running on port ${PORT}.`);
-// });
-
-// Test the database connection
-Model.sequelize
-  .authenticate()
-  .then(() => {
-    console.log(config);
-    console.log(`"${env}" database connection established successfully!`);
-
-    // Sync the models with the database
-    // return Model.sequelize.sync({ alter: true }); // Use `force: true` for dropping tables and recreating them
-  })
-  .then(() => {
-    // Start the server
-    app.listen(PORT, () => {
-      console.warn(`Server is running on --->  http://localhost:${PORT}/api/`);
-    });
-  })
-  .catch((err) => {
-    console.error('Unable to connect to the database:', err);
-  });
+server.listen(PORT, () => {
+  console.warn(`LMS is running on port ${PORT}.`);
+});
